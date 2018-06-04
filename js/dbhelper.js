b2747f5cb50b11f9c8e8a018810fa0e122ef9360
paulluquin <34561163+paulluquin@users.noreply.github.com>
2018-06-04 18:46:18 -0400

    Overall Site Layout Improvements
    Size/Shape and layout changes to work with different devices

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -154,6 +154,13 @@ class DBHelper {
   }
 
   /**
+   * Restaurant image alt text.
+   */
+  static imageAltForRestaurant(restaurant) {
+    return (`Sample Menu Image from ${restaurant.name}`);
+  }
+
+  /**
    * Map marker for a restaurant.
    */
   static mapMarkerForRestaurant(restaurant, map) {
