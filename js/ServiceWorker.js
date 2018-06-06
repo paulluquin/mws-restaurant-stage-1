977b61bfc6f510973a084e997d06a428f479df73
paulluquin <34561163+paulluquin@users.noreply.github.com>
2018-06-06 07:23:43 -0400

    Review #1
    Fixed typo in view port, change images for small viewport, added aria references, started service worker implementation

--- js/ServiceWorker.js
+++ js/ServiceWorker.js
@@ -1,3 +1,19 @@
-self.addEventListener('fetch', function(event) {
-  console.log(event.request)
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/styles.css',
+  '/css/styles_large.css',
+  '/restaurants.json',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
