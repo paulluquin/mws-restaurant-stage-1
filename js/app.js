4ad6992676dd9c71af5d6b08f68f45d90d1dc33a
paulluquin <34561163+paulluquin@users.noreply.github.com>
2018-06-13 19:32:05 -0400

    SW Changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-console.log('HELLO HERE');
+
 /* Service worker example from Google
 https://developers.google.com/web/fundamentals/primers/service-workers/*/
+
 if ('serviceWorker' in navigator) {
 
-  navigator.serviceWorker.register('/js/ServiceWorker.js').then(function(registration) {
+  navigator.serviceWorker.register('ServiceWorker.js').then(function(registration) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ', registration.scope);
   }, function(err) {
