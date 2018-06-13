d1b30695fd21b40ddb0f3bb2a74244619dfc59dc
paulluquin <34561163+paulluquin@users.noreply.github.com>
2018-06-12 22:12:50 -0400

    More SW changes

--- js/ServiceWorker.js
+++ js/ServiceWorker.js
@@ -4,12 +4,11 @@ var urlsToCache = [
   '/css/styles.css',
   '/css/styles_large.css',
 
-  '/js/restaurants_info.json',
-  '/js/app.js',
+  '/data/restaurants.json',
+
   '/js/dbhelper.js',
   '/js/main.js',
   '/js/restaurant_info.js',
-  '/js/ServiceWorker.js',
 
   '/img/1.jpg',
   '/img/2.jpg',
@@ -22,11 +21,8 @@ var urlsToCache = [
   '/img/9.jpg',
   '/img/10.jpg',
 
-  '/data/restaurants.json',
-
   '/index.html',
-  '/restaurant.html',
-  '/script/main.js'
+  '/restaurant.html'
 ];
 
 self.addEventListener('install', function(event) {
@@ -36,6 +32,8 @@ self.addEventListener('install', function(event) {
       .then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
+      }).then(function() {
+        return self.skipWaiting();
       })
   );
 });
@@ -59,17 +57,16 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-
 console.log('Fetch');
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    cache.match(event.request).then(function (response) {
-      if (response) {
-        console.log (event.request.url);
-        return response;
-      }
-      console.log(event.request.url);
-      return fetch(event.request);
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open(cacheVersion).then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
     })
   );
-});
+})
--- js/app.js
+++ js/app.js
@@ -2,30 +2,12 @@ console.log('HELLO HERE');
 /* Service worker example from Google
 https://developers.google.com/web/fundamentals/primers/service-workers/*/
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/js/ServiceWorker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+
+  navigator.serviceWorker.register('/js/ServiceWorker.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }, function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  })
 }
-
-
-
-/*
-this.addEventListener('fetch', function(event) {
-  console.log('Service Worker Start');
-  console.log(event.request);
-  if(!navigator.serviceWorker) return;
-
-  navigator.serviceWorker.register('js/serviceworker.js').then(function() {
-    console.log("Registration Worked");
-  }).catch(function() {
-    console.log("Registration Failed")
-  });
-});
-console.log('Hello there');
-*/
